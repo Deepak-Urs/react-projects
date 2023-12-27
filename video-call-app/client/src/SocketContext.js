7aced564dc63b6a8bee07dabaa38714bec8af43d
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-12-26 23:01:39 -0800

    updated SocketContext logic

--- video-call-app/client/src/SocketContext.js
+++ video-call-app/client/src/SocketContext.js
@@ -14,8 +14,14 @@ const ContextProvider = ({ children }) => {
     const [me, setMe] = useState('')
     const [call, setCall] = useState({})
 
+    const [callAccepted, setCallAccepted] = useState(false)
+    const [callEnded, setCallEnded] = useState(false)
+    const [name, setName] = useState('')
+
 
     const myVideo = useRef()
+    const userVideo = useRef()
+    const connctionRef = useRef()
 
     
     useEffect(() => {
@@ -43,14 +49,68 @@ const ContextProvider = ({ children }) => {
     }, [])
 
     const answerCall = () => {
+        setCallAccepted(true)
 
+        // helps with the video call connection and works like socket
+        const peer = new Peer({ initiator: false , trickle: false, stream })
+        
+        peer.on('signal', (data) => {
+            socket.emit('answercall', {signal: data, to: call.from})
+        })
+
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream
+        })
+
+        peer.signal(call.signal)
+
+        connctionRef.current = peer
     }
 
-    const callUser = () => {
+    const callUser = (id) => {
+        const peer = new Peer({ initiator: true , trickle: false, stream })
+        
+        peer.on('signal', (data) => {
+            socket.emit('calluser', {userToCall: id, signalData: data, from: me, name})
+        })
 
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream
+        })
+
+        socket.on('callaccepted', (signal) => {
+            setCallAccepted(true)
+
+            peer.signal(signal)     
+        })
+
+        connctionRef.current = peer
     }
 
-    const leaverCall = () => {
-
+    const leaveCall = () => {
+        setCallEnded(true);
+        connctionRef.current.destroy();
+        window.location.reload();
     }
+
+    return (
+        <SocketContext.Provider value={{
+            call,
+            callAccepted,
+            myVideo,
+            userVideo,
+            stream,
+            name,
+            setName,
+            callEnded,
+            me,
+            callUser,
+            leaveCall,
+            answerCall
+        }}>
+            {children}
+        </SocketContext.Provider>
+    )
 }
+
+export {ContextProvider, SocketContext}
