28307c533ec9b684fe6f749515af42b362905f8a
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-12-28 14:35:13 -0800

    Notifications component updated

--- video-call-app/client/src/components/Notifications.jsx
+++ video-call-app/client/src/components/Notifications.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
+
+import { SocketContext } from '../SocketContext'
+import { useContext } from 'react'
 
 const Notifications = () => {
+  const { answerCall, call, callAccepeted} = useContext(SocketContext);
+
+
   return (
-    <div>
-      Notifications
-    </div>
+    <>
+      {call.isReceivedCall && !callAccepeted && (
+        <div style={{display:'flex', justifyContent: 'center'}}>
+          <h1>{callAccepeted.name} is calling:</h1>
+          <Button variant='="contained' color='primary' onClick={answerCall} >
+            Answer
+          </Button>
+        </div>
+      )}
+    </>
   )
 }
 
