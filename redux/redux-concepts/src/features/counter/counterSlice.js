34c2a721b954786abaf33d4d9e031ccec8e6c0db
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-01 15:53:45 -0800

    Counter-app with store,reducer,action, dispatcher

---
+++ redux/redux-concepts/src/features/counter/counterSlice.js
@@ -0,0 +1,24 @@
+// 2 - REDUCER:  A slice is a collection of reducer Logic
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+    count: 0
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        // 3 - ACTIONS - actions
+        increment: (state) => {
+            state.count += 1
+        },
+        decrement: (state) => {
+            state.count -= 1
+        }
+    }
+})
+
+export const {increment, decrement} = counterSlice.actions;
+
+export default counterSlice.reducer
