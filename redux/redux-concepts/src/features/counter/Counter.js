1a2cf08bd7f78a3625705b4e96ee5be9b0b2105c
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-02 13:29:18 -0800

    added the reset and incrementValue actions with css

--- redux/redux-concepts/src/features/counter/Counter.js
+++ redux/redux-concepts/src/features/counter/Counter.js
@@ -1,17 +1,33 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from './counterSlice'
+import { increment, decrement, reset, incrementByAmount } from './counterSlice';
+import { useState } from 'react';
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch()
+    const [incrementAmount, setIncrementAmount] = useState(0)
+
+    const addValue = Number(incrementAmount) || 0;
+
+    const resetAll = () => {
+        setIncrementAmount(0)
+        dispatch(reset())
+    }
 
     return (
         <section>
+            <h6 style={{display:"flex", alignContent: "center", justifyContent: "center"}}>Count Value application</h6>
             <p>{count}</p>
             <div>
                 {/* 4 - Dispatcher */}
                 <button onClick={() => dispatch(increment())}>+</button>
                 <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
+
+            <input type='text' value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+            <div>
+                <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Value</button>
+                <button onClick={resetAll}>Reset</button>
             </div>
         </section>
     )
