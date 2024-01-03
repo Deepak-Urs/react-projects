72630f2a3a7bf0e5c348f3420493782d3cec7a2e
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-03 13:34:32 -0800

    Notes app: Base Structure

---
+++ redux/notes app/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="App">
+      "Hello App"
+    </main>
+  );
+}
+
+export default App;
---
+++ redux/notes app/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+// 1 - STORE - global store
+export const store = configureStore({
+    reducer: {
+        
+    }
+})
