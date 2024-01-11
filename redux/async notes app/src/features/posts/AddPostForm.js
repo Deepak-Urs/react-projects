40cd4c98942c981609e37b1da551aa6625537786
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-11 13:29:24 -0800

    addNewPost operation

--- redux/async notes app/src/features/posts/AddPostForm.js
+++ redux/async notes app/src/features/posts/AddPostForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { postAdded, addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
@@ -10,28 +10,37 @@ const AddPostForm = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const [userId, setUserId] = useState('')
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
     const users = useSelector(selectAllUsers)
 
     const onTitleChanged = (e) => setTitle(e.target.value);
     const onContentChanged = (e) => setContent(e.target.value);
-    const onAuthorChanged = (e) => {console.log('e.target.value seen', e.target.value); setUserId(e.target.value)};
+    const onAuthorChanged = (e) => { console.log('e.target.value seen', e.target.value); setUserId(e.target.value) };
 
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
 
     const onSavePostClicked = () => {
-        console.log('onSavePostClicked is called');
-        if(title && content){
-            dispatch(postAdded(title, content, parseInt(userId)))
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, body: content, userId })).unwrap()
 
-            setTitle('')
-            setContent('')
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.error('Failed to save the post', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
     }
 
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
 
     const userOptions = users.map(user => (
-        <option key={user.id} value ={user.id}>
+        <option key={user.id} value={user.id}>
             {user.name}
         </option>
     ))
--- redux/async notes app/src/features/posts/postsSlice.js
+++ redux/async notes app/src/features/posts/postsSlice.js
@@ -14,7 +14,16 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     try {
         const response = await axios.get(POSTS_URL)
         return [...response.data];
-    } catch(err) {
+    } catch (err) {
+        return err.message;
+    }
+})
+
+export const addNewPost = createAsyncThunk('post/addNewPost', async (initialPost) => {
+    try {
+        const response = await axios.post(POSTS_URL, initialPost);
+        return response.data;
+    } catch (err) {
         return err.message;
     }
 })
@@ -23,14 +32,14 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        postAdded: { 
+        postAdded: {
             reducer(state,
-                 action) {
-            // inner.js mutates the state without mutating the state as a whole(pseudo)
+                action) {
+                // inner.js mutates the state without mutating the state as a whole(pseudo)
                 state.posts.push(action.payload);
             },
             prepare(title,
-                 content, userId) {
+                content, userId) {
                 return {
                     payload: {
                         id: nanoid(),
@@ -44,13 +53,13 @@ const postsSlice = createSlice({
                             rocket: 0,
                             thumbsUp: 0,
                             wow: 0
-                         }
+                        }
                     }
                 }
             }
         },
         reactionAdded(state, action) {
-            const {postId, reaction} = action.payload
+            const { postId, reaction } = action.payload
             const existingPayload = state.posts.find(post => post.id === postId)
             if (existingPayload) {
                 existingPayload.reactions[reaction]++;
@@ -62,27 +71,40 @@ const postsSlice = createSlice({
         builder.addCase(fetchPosts.pending, (state, action) => {
             state.status = 'loading'
         })
-        .addCase(fetchPosts.fulfilled, (state,action) => {
-            state.status = 'succeded'
-            let min = 1
-            const loadedPosts = action.payload.map(post => {
-                post.date = sub(new Date(), {minutes:min++}).toISOString();
-                post.reactions = {
-                    thumbsUp: 0,
-                    hooray: 0,
-                    heart: 0,
-                    rocket: 0,
-                    eyes: 0
-                }
-                return post;
-            });
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeded'
+                let min = 1
+                const loadedPosts = action.payload.map(post => {
+                    post.date = sub(new Date(), { minutes: min++ }).toISOString();
+                    post.reactions = {
+                        thumbsUp: 0,
+                        hooray: 0,
+                        heart: 0,
+                        rocket: 0,
+                        eyes: 0
+                    }
+                    return post;
+                });
 
-            state.posts = state.posts.concat(loadedPosts)
-        })
-        .addCase(fetchPosts.rejected, (state,action) => {
-            state.status = 'failed'
-            state.error = action.error.message
-        })
+                state.posts = state.posts.concat(loadedPosts)
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                action.payload.userId = Number(action.payload.userId)
+                action.payload.date = new DataView().toISOString()
+                action.payload.reactions = {
+                        thumbsUp: 0,
+                        hooray: 0,
+                        heart: 0,
+                        rocket: 0,
+                        eyes: 0
+                }
+                console.log(action.payload);
+                state.posts.push(action.payload)
+            })
     }
 })
 
